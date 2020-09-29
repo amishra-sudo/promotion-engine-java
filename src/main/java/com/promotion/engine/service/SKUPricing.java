e3e4df466afa6ed69b2e76327b8a3188a1e0ccfd
abmishra <amishra@pieriandx.com>
2020-09-29 21:58:09 +0530

    Added configuration

--- src/main/java/com/promotion/engine/service/SKUPricing.java
+++ src/main/java/com/promotion/engine/service/SKUPricing.java
@@ -1,9 +1,39 @@
 package com.promotion.engine.service;
 
+import com.promotion.engine.domain.SKUModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SKUPricing {
+
+    @Autowired
+    SKUModel sku;
+
+    public int getPriceForItem(String itemCode) {
+        int price;
+        switch (itemCode) {
+            case "A":
+                price = sku.getaPrice();
+                break;
+            case "B":
+                price = sku.getbPrice();
+                break;
+            case "C":
+                price = sku.getcPrice();
+                break;
+            case "D":
+                price = sku.getdPrice();
+                break;
+            default:
+                price = 0;
+                break;
+
+        }
+        return price;
+
+
+    }
 
 
 }
