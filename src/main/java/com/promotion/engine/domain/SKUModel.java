0ea0a1ec18fd14cc94a7628124574dc06b9d250b
abmishra <amishra@pieriandx.com>
2020-09-30 00:48:09 +0530

    Added configuration

--- src/main/java/com/promotion/engine/domain/SKUModel.java
+++ src/main/java/com/promotion/engine/domain/SKUModel.java
@@ -1,57 +1,16 @@
 package com.promotion.engine.domain;
 
 
-import com.promotion.engine.util.PromotionalMethodEnum;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
 
 @ConfigurationProperties(prefix = "sku.config")
 @Component
 public class SKUModel {
-
-
-    private String aName;
-    private String bName;
-    private String cName;
-    private String dName;
     private Integer aPrice;
     private Integer bPrice;
     private Integer cPrice;
     private Integer dPrice;
-
-    public String getaName() {
-        return aName;
-    }
-
-    public void setaName(String aName) {
-        this.aName = aName;
-    }
-
-    public String getbName() {
-        return bName;
-    }
-
-    public void setbName(String bName) {
-        this.bName = bName;
-    }
-
-    public String getcName() {
-        return cName;
-    }
-
-    public void setcName(String cName) {
-        this.cName = cName;
-    }
-
-    public String getdName() {
-        return dName;
-    }
-
-    public void setdName(String dName) {
-        this.dName = dName;
-    }
-
     public Integer getaPrice() {
         return aPrice;
     }
@@ -84,17 +43,4 @@ public class SKUModel {
         this.dPrice = dPrice;
     }
 
-    @Override
-    public String toString() {
-        return "SKUModel{" +
-                "aName='" + aName + '\'' +
-                ", bName='" + bName + '\'' +
-                ", cName='" + cName + '\'' +
-                ", dName='" + dName + '\'' +
-                ", aPrice=" + aPrice +
-                ", bPrice=" + bPrice +
-                ", cPrice=" + cPrice +
-                ", dPrice=" + dPrice +
-                '}';
-    }
 }
